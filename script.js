6279f43517f238b9b3ead15cb56d524f2803b345
Anushtha Sharma <159756259+Anushtha30@users.noreply.github.com>
2024-11-25 20:19:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Example: Adding an alert to a button click
+document.querySelector('.hero-section button').addEventListener('click', function() {
+  alert('Welcome to our University!');
+});
